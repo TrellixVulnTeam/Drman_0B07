94d9eec61eb01f38427b9f5aa54f60dfee54e101
avahmh <avaa.torabli@gmail.com>
2019-05-16 08:17:39 +0430

    production settings

--- Drman/wsgi.py
+++ Drman/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Drman.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Drman.settings.dev")
 
 application = get_wsgi_application()
